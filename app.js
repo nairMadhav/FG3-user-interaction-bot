ab5750f1b29bd7fa2937e1b343367e02a367206d
nairmadhav <maddy.nair01@gmail.com>
2021-09-23 19:57:55 +0530

    [#2] Added more description to the userDesc string and also added file IO for getting and downloading user audio

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('dotenv').config();
 const BusBoy=require('busboy')
 const FileUpload=require('express-fileupload');
+const fs=require('fs')
 
 
 //middleware 
@@ -11,18 +12,20 @@ const app=express();
 app.use(FileUpload())
 
 //mock db from userData.json to mimick order details of users
-const userDb=require('./userData.json')
+const userDb=require('./userData.json');
+const { fstat } = require('fs');
 console.log(`There are ${userDb.users.length} users in the mock DB`)
 
 //=============================================================
 
-app.get('/upload',(req,res)=>{
+app.get('/upload-audio',(req,res)=>{
     /* The IOS frontend should send the audio stream 
         as an audio file here
         ...
         This is just a test to check if the file io works
     */
     res.sendFile(__dirname+'/file-io-test.htm')
+    
 })
 
 app.post('/upload',(req,res)=>{
@@ -44,6 +47,46 @@ app.post('/upload',(req,res)=>{
     })
 
 })
+
+app.get('/get-user-audio',(req,res)=>{
+    const audioPath=__dirname+'/uploadedFiles/';
+    const files=fs.readdirSync(audioPath);
+    const audio=files[0]
+    console.log(audio)
+    
+    res.download(audioPath+audio,(e)=>{
+        if(e){
+            console.log(e)
+        } 
+        //The callback function deletes the audio after it has been downloaded
+        fs.unlink(__dirname+'/uploadedFiles/'+audio,(e)=>{
+                if(e){
+                    console.log(e)
+                }
+                console.log('deleted audio from the server')
+        });
+    })
+   
+    
+    
+    
+})
+
+/*=======User JSON Description===============
+ * User Object:
+  {
+    username: String,
+    _id: Number,
+    orders: [ [Order Object]* ]
+  }
+ * Order object:
+  {
+    "id":Number,
+    "status":Number,
+    "desc":String
+  }
+ */
+
 //get JSON of specific userx
 app.get(`/api/:uid`,(req,res)=>{
     const user=userDb.users.filter(user=>user._id==req.params.uid)
@@ -52,14 +95,14 @@ app.get(`/api/:uid`,(req,res)=>{
 
 
 //get the number of orders a user has
-app.get(`/api/numOrders/:uid`,(req,res)=>{
+app.get(`/api/num-orders/:uid`,(req,res)=>{
     const user=userDb.users.filter(user=>user._id==req.params.uid)
     res.status(200).send(user[0].orders.length.toString())
 })
 
 
 //Get the number of orders for a specific user that have a specific status code
-app.get(`/api/numOrders/:uid/:status`,(req,res)=>{
+app.get(`/api/num-orders/:uid/:status`,(req,res)=>{
     /*
         status codes:
         0-Delivered
@@ -75,6 +118,8 @@ app.get(`/api/numOrders/:uid/:status`,(req,res)=>{
     console.log(orders.length)
     res.status(200).send(orders.length.toString())
 })
+//================================The Important part=====================================
+//Sends user description in a string to the bot to give context
 app.get(`/api/get-desc/:uid`,(req,res)=>{
 
     const user=userDb.users.filter(user=>user._id==req.params.uid);
@@ -82,10 +127,17 @@ app.get(`/api/get-desc/:uid`,(req,res)=>{
     console.log(user)
     console.log(orders.length)
     var desc=`My name is ${user[0].username}.\n I have ordered ${orders.length.toString()} items.\n`;
-    
+    //Adding the order status to the description string
     for(i=0;i<orders.length;i++){
         desc+=(` My ${orders[i].desc} `)
         switch (orders[i].status) {
+            /*
+                status codes:
+                0-Delivered
+                100-Out for delivery
+                200-Shipped
+                300-Order Confirmed
+            */
             case 0:
                 desc+= `has been delivered.\n`
                 break;
@@ -103,6 +155,36 @@ app.get(`/api/get-desc/:uid`,(req,res)=>{
                 break;
         }
     }
+    //Adding number of orders with respective delivery status
+    var delivered=0
+    var out_for_delivery=0
+    var shipped=0
+    var confirmed=0
+    for(i=0;i<orders.length;i++){
+        status=orders[i].status;
+        
+        switch (status) {
+            case 0:
+                delivered+=1
+                break;
+            case 100:
+                out_for_delivery+=1
+                break;
+            case 200:
+                shipped+=1
+                break;
+            case 300:
+                confirmed+=1
+                break;
+            default:
+                break;
+        }
+    }
+    //splitting for better readability. Im not retarded
+    desc+=`${delivered} order(s) have been delivered.\n`
+    desc+=` ${out_for_delivery} order(s) are currently out for delivery.\n`
+    desc+=` ${shipped} order(s) have been shipped.\n`
+    desc+=` ${confirmed} order(s) have been confirmed.`
     res.status(200).send(desc);
     console.log(desc)
 })
